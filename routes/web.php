0d93fe2be72108e93c012828b396e67ce7914b1d
bettercallkyaw <bettercallkyaw@outlook.com>
2021-11-09 15:50:24 +0630

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,31 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/posts',function(){
-    return 'Post List';
-});
+// Route::get('/posts',function(){
+//     return 'Post List';
+// });
 
-Route::get('/posts/show',function(){
-    return 'Post Show';
-})->name('posts.show');
+// Route::get('/posts/show',function(){
+//     return 'Post Show';
+// })->name('posts.show');
 
 //dynamic route
-Route::get('/posts/show/{id}',function($id){
-    return "Post Show-$id";
-});
+// Route::get('/posts/show/{id}',function($id){
+//     return "Post Show-$id";
+// });
 
-Route::get('/posts/more',function(){
-    //return redirect('/posts/show');
-    return redirect()->route('posts.show');
-});
+// Route::get('/posts/more',function(){
+//     //return redirect('/posts/show');
+//     return redirect()->route('posts.show');
+// });
+
+Route::get('/',[PostController::class,'index']);
+Route::get('/posts',[PostController::class,'index']);
+Route::get('/posts/show/{id}',[PostController::class,'show']);
